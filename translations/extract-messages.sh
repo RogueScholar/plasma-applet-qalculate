83786878864650a2ddb34126f030350ed52da68c
Restyled.io <commits@restyled.io>
2020-01-15 19:29:08 +0000

    Restyled by shellharden

--- translations/extract-messages.sh
+++ translations/extract-messages.sh
@@ -6,14 +6,14 @@ BUGADDR="https://github.com/dschopf/qalculate/issues"
 
 echo "Extracting messages"
 
-find ${BASEDIR} -name '*.qml' | sort > infiles.list
+find "$BASEDIR" -name '*.qml' | sort > infiles.list
 
 xgettext --from-code=UTF-8 -C -L JavaScript -kde -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3 -ktr2i18n:1 \
 	-kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kaliasLocale -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3 \
-	--msgid-bugs-address="${BUGADDR}" \
+	--msgid-bugs-address="$BUGADDR" \
 	--files-from=infiles.list \
-	--package-name="${PROJECT}" \
-	-o po/${PROJECT}.pot || { echo "error while calling xgettext. aborting."; exit 1; }
+	--package-name="$PROJECT" \
+	-o po/"$PROJECT".pot || { echo "error while calling xgettext. aborting."; exit 1; }
 
 echo "Done extracting messages"
 
@@ -21,9 +21,9 @@ echo "Merging translations"
 
 catalogs=`find ./po -name '*.po'`
 
-for cat in $catalogs; do
+for cat in "$catalogs"; do
   echo "$cat"
-  msgmerge -o "$cat".new "$cat" po/${PROJECT}.pot
+  msgmerge -o "$cat".new "$cat" po/"$PROJECT".pot
   mv "$cat".new "$cat"
 done
 

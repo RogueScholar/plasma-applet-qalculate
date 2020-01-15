0a028eda0df103f6dbc4cd0431d3cc976ea90724
Restyled.io <commits@restyled.io>
2020-01-15 19:29:09 +0000

    Restyled by shfmt

--- translations/extract-messages.sh
+++ translations/extract-messages.sh
@@ -1,25 +1,28 @@
 #!/usr/bin/env bash
 
-PROJECT="plasma_applet_qalculate"	# project name
-BASEDIR="../plasmoid"			# root of translatable sources
+PROJECT="plasma_applet_qalculate" # project name
+BASEDIR="../plasmoid"             # root of translatable sources
 BUGADDR="https://github.com/dschopf/qalculate/issues"
 
 echo "Extracting messages"
 
-find "$BASEDIR" -name '*.qml' | sort > infiles.list
+find "$BASEDIR" -name '*.qml' | sort >infiles.list
 
 xgettext --from-code=UTF-8 -C -L JavaScript -kde -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3 -ktr2i18n:1 \
-	-kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kaliasLocale -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3 \
-	--msgid-bugs-address="$BUGADDR" \
-	--files-from=infiles.list \
-	--package-name="$PROJECT" \
-	-o po/"$PROJECT".pot || { echo "error while calling xgettext. aborting."; exit 1; }
+  -kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kaliasLocale -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3 \
+  --msgid-bugs-address="$BUGADDR" \
+  --files-from=infiles.list \
+  --package-name="$PROJECT" \
+  -o po/"$PROJECT".pot || {
+  echo "error while calling xgettext. aborting."
+  exit 1
+}
 
 echo "Done extracting messages"
 
 echo "Merging translations"
 
-catalogs=`find ./po -name '*.po'`
+catalogs=$(find ./po -name '*.po')
 
 for cat in "$catalogs"; do
   echo "$cat"
